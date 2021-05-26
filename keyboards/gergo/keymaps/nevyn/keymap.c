f4e664e27bb9fc1b04e9d04a45c8f47f6cd8f4bc
Nevyn Bengtsson <nevyn@alloverse.com>
2021-05-26 13:12:51 +0200

    shift belongs on bottom row

--- keyboards/gergo/keymaps/nevyn/keymap.c
+++ keyboards/gergo/keymaps/nevyn/keymap.c
@@ -49,8 +49,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [BASE] = LAYOUT_gergo(
     KC_ESC,                 KC_Q,  KC_W,   KC_E,   KC_R, KC_T,                                          KC_Y,    KC_U, KC_I, KC_O,   KC_P,    KC_PIPE,
-    KC_RSFT,               KC_A,  KC_S,   KC_D,   KC_F, KC_G, KC_BTN2,                       KC_RALT,  KC_H,    KC_J, KC_K, KC_L,   KC_SCLN, KC_QUOT,
-    KC_LCTRL,                KC_Z,  KC_X,   KC_C,   KC_V, KC_B, KC_BTN1, KC_BTN3,    TG(MUTE), KC_PGDN,  KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_MINS,
+    KC_LCTRL,               KC_A,  KC_S,   KC_D,   KC_F, KC_G, KC_BTN2,                       KC_RALT,  KC_H,    KC_J, KC_K, KC_L,   KC_SCLN, KC_QUOT,
+    KC_RSFT,                KC_Z,  KC_X,   KC_C,   KC_V, KC_B, KC_BTN1, KC_BTN3,    TG(MUTE), KC_PGDN,  KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_MINS,
                           KC_LGUI, KC_LALT , LT(NUMB, KC_SPC), LT(SYMB, KC_TAB),     LT(SYMB, KC_ENT), LT(NUMB, KC_SPC), KC_BSPC, MO(MOUS)
     ),
 /* Keymap 1: Symbols layer
